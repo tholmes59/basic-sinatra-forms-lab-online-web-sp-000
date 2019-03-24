d6dea05a326580c707326d8962553fff8d51b137
Tom Holmes <tholmes59@gmail.com>
2019-03-24 19:04:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ class App < Sinatra::Base
   
   post '/newteam' do 
     @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    
     erb :team
   end 
 
